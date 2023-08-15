cd9947634e620fb13728d92540acc89b24e00b44
suryadip38 <91530149+suryadip38@users.noreply.github.com>
2023-08-15 11:31:00 +0545

    g

--- Assets/Script/PhotonManager.cs
+++ Assets/Script/PhotonManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 using Photon.Realtime;
-using Photon.Pun.Demo.Cockpit;
-using UnityEngine.WSA;
+using UnityEditor.Experimental.GraphView;
+//using Photon.Pun.Demo.Cockpit;
+//using UnityEngine.WSA;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
@@ -20,19 +20,25 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     public GameObject RoomCreatePanel;
     public GameObject ConnectingPanel;
     public GameObject RoomlistPanel;
-   
+
+    private Dictionary<string, RoomInfo> roomListData;
+    public GameObject roomListPrefab;
+    public GameObject roomListParent;
+
+    private Dictionary<string,GameObject> roomListGameObject;
 
     #region UnityMehtods
     // Start is called before the first frame update
     void Start()
     {
         ActiiveMyPanel(PlayerNamePanel.name);
+        roomListData = new Dictionary<string, RoomInfo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Network state :"+PhotonNetwork.NetworkClientState);
+        Debug.Log("Network state :"+PhotonNetwork.NetworkClientState); 
     }
     #endregion
 
@@ -53,29 +59,42 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     }
 
     public void OnClickRoomCreate()
-    {
-       string roomName = roomNameText.text; 
-        if (!string.IsNullOrEmpty(roomName)) 
         {
-            roomName = roomName + Random.Range(0, 1000);                
+            string roomName = roomNameText.text;
+            if (string.IsNullOrEmpty(roomName))
+            {
+              roomName = roomName + Random.Range(0, 1000);
+            }
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = (byte)int.Parse(MaxPlayer.text);
+
+           /* if (int.TryParse(MaxPlayer.text, out int maxPlayers))
+            {
+                roomOptions.MaxPlayers = (byte)maxPlayers;
+            }
+            else
+            {
+                Debug.LogError("Invalid max players value!");
+                return;
+            }*/
+
+            PhotonNetwork.CreateRoom(roomName, roomOptions);
         }
-        RoomOptions roomOptions = new RoomOptions();
-        RoomOptions.MaxPlayer=(byte)int.Parse(MaxPlayer.text);
-        PhotonNetwork.CreateRoom(roomName,roomOptions);
-    }
+
 
     public void OnCancelClick()
+
     {
         ActiiveMyPanel(LobbyPanel.name);
     }
     public void RoomListbtnClicked()
     {
-        if (!PhotonNetwork.InLobby) ;
+        if (!PhotonNetwork.InLobby)
         {
             PhotonNetwork.JoinLobby();
             
         }
-        ActiiveMyPanel(RoomlistPanel.name) ;
+        ActiiveMyPanel(RoomlistPanel.name);
     }
     #endregion
 
@@ -96,7 +115,14 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     }
     public override void OnCreatedRoom()
     {
-        Debug.Log(PhotonNetwork.CurrentRoom.Name + "Is created");
+        // if (PhotonNetwork.CurrentRoom != null)
+        // {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + "Is created!");
+      //  }
+       /* else
+        {
+            Debug.LogError("PhotonNetwork.CurrentRoom is null!");
+        }*/
     }
 
 
@@ -110,6 +136,38 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         foreach(RoomInfo rooms in roomList)
         {
             Debug.Log("RoomName :" + rooms.Name);
+            if(rooms.IsOpen|| rooms.IsVisible||rooms.RemovedFromList)
+            {
+                if(roomListData.ContainsKey(rooms.Name))
+                {
+                    roomListData.Remove(rooms.Name);
+                }
+                else{
+                    if (roomListData.ContainsKey(rooms.Name))
+                    {
+                        roomListData[rooms.Name] = rooms;
+                      
+                    }
+                    else
+                    {
+                        roomListData.Add(rooms.Name, rooms);
+                    }
+                }
+            }
+            // Generate List Item
+            foreach(RoomInfo roomItem in roomListData.Values)
+            {
+                GameObject roomListItemObject = Instantiate(roomListPrefab);
+                roomListItemObject.transform.SetParent(roomListParent.transform);
+                roomListItemObject.transform.localScale = Vector3.one;
+                //room name player number Button room join
+
+                roomListItemObject.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = roomItem.Name;
+                roomListItemObject.transform.GetChild(1).gameObject.GetComponent<TextMeshPro>().text = roomItem.PlayerCount +"/"+roomItem.MaxPlayers;
+                roomListItemObject.transform.GetChild(2).gameObject.GetComponent<Button>().onClick.AddListener(() => RoomJoinFromList(roomItem.Name));
+                roomListGameObject.Add(roomItem.Name,roomListItemObject);
+
+            }
         }
     }
     #endregion
@@ -118,6 +176,24 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     #region Public_Methods
 
+    public void RoomJoinFromList(string roomName)
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ClearRoomList()
+    {
+        if(roomListGameObject.Count>0)
+        foreach(var v in roomListGameObject.Values)
+        {
+            Destroy(v);
+        }
+    }
+
     public void ActiiveMyPanel(string PanelName)
     {
         LobbyPanel.SetActive(PanelName.Equals(LobbyPanel.name));
@@ -125,6 +201,8 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         RoomCreatePanel.SetActive(PanelName.Equals(RoomCreatePanel.name));
         ConnectingPanel.SetActive(PanelName.Equals(ConnectingPanel.name));
         RoomlistPanel.SetActive(PanelName.Equals(RoomlistPanel.name));
+
+        Debug.Log("everthing is called");
     }
 
     #endregion
